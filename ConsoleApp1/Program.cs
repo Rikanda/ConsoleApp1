3f128a72d8ceab7f6938aefc33e36cd46069f0f8
Natalya <soltk7@yandex.ru>
2021-02-04 23:06:25 +0300

    Сделаны задания 1-3, задания со звездочками сделать не успела

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -22,6 +22,33 @@ namespace ConsoleApp1
 
             logtime.Logs();
 
+            // задание 3, запись бинарного файла
+
+            
+            string txt2 = "somebytes.bin";
+
+            Console.Write("Ведите набор чисел через пробел: ");
+            string numbers = Console.ReadLine();
+
+            string[] ns = numbers.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            byte[] test = new byte[ns.Length];
+
+            for (int i = 0; i < ns.Length; i++)
+            { 
+                test[i] = Convert.ToByte(ns[i]);
+                
+            }
+
+            File.WriteAllBytes(txt2, test);
+
+
+            
+
+           
+           
+            
+
 
         }
     }
